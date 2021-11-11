e3f958691b424163cb0644bfe745d85a887c27fc
RohithKondle <67865663+rohith1998-123@users.noreply.github.com>
2021-11-10 20:28:52 -0600

    Added the post configurations

--- controllers/flights.js
+++ controllers/flights.js
@@ -44,3 +44,27 @@ exports.flight_view_all_Page = async function(req, res) {
     }   
 }; 
  
+// Handle Costume create on POST. 
+exports.flight_create_post = async function(req, res) { 
+    console.log(req.body) 
+    let document = new Flights(); 
+    // We are looking for a body, since POST does not have query parameters. 
+    // Even though bodies can be in many different formats, we will be picky 
+    // and require that it be a json object 
+    // {"costume_type":"goat", "cost":12, "size":"large"} 
+    console.log('the first'+req.body.flightType)
+    console.log('the second'+ req.body.price)
+    document.flightType = req.body.flightType;
+    console.log(document.flightType)
+    document.price = req.body.price; 
+    document.capacity = req.body.capacity; 
+    try{ 
+        let result = await document.save(); 
+        res.send(result); 
+    } 
+    catch(err){ 
+        res.status(500); 
+        res.send(`{"error": ${err}}`); 
+    }   
+}; 
+ 
